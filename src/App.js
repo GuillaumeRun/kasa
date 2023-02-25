327b1d0a460d5b63498c992a9badcec2625fb00a
Guillaume.YRN <guillaumepause@proton.me>
2023-02-25 19:05:32 +0400

    modif app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import NotFound from "./pages/notFound/NotFound";
 import About from "./pages/about/About";
 import Accomodation from "./pages/accomodation/Accomodation";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/accomodation/:id",
-    element: <Accomodation />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/accomodation/:id" element={<Accomodation />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
