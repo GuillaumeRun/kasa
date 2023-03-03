3363e9a2e89a9f761e08d92c69edf5cee0c8998a
Guillaume.YRN <guillaumepause@proton.me>
2023-03-03 10:34:10 +0400

    add notfound and about pages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import NotFound from "./pages/notFound/NotFound";
 import About from "./pages/about/About";
-import Accomodation from "./pages/accomodation/Accomodation";
+import Accommodation from "./pages/accommodation/Accommodation";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/accomodation/:id" element={<Accomodation />} />
+        <Route path="/accommodation/:id" element={<Accommodation />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import './notFound.scss'
+import Header from "../../components/header/Header";
+import { Link } from 'react-router-dom';
 
-function NotFound() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to our website!</p>
-    </div>
-  );
+export default function NotFound() {
+	return (
+		<div className='notFound'>
+			<Header />
+			<div className="notFound_infos">
+				<h1 className='notFound_infos_title'>404</h1>
+				<p className='notFound_infos_content'>La page que vous recherchez n'existe pas.</p>
+			</div>
+				<Link className='notFound_infos_return' to='/'>Retourner sur la page d'accueil</Link>
+		</div>
+	)
 }
-
-export default NotFound;
