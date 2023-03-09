80d69b5814ddf0aa61a1d87404a2ef6ab7c126b6
Guillaume.YRN <guillaumepause@proton.me>
2023-03-09 19:41:40 +0400

    add carrousel and page

--- src/pages/accommodation/Accommodation.jsx
+++ src/pages/accommodation/Accommodation.jsx
@@ -1,13 +1,94 @@
+import "./accommodation.scss";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import datas from "../../data/data";
+import Header from "../../components/header/Header";
+import Slider from "../../components/carrousel/Carrousel";
+import Footer from "../../components/footer/Footer";
+import Collapse from "../../components/collapse/Collapse";
+import greyStar from "../../assets/grey_star.png";
+import redStar from "../../assets/red_star.png";
 
-import React from 'react';
+export default function Accommodation() {
+  const [imageSlider, setImageSlider] = useState([]);
+  const navigate = useNavigate();
 
-function Accommodation() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to our website!</p>
-    </div>
+  const { id: idAccommodation } = useParams();
+
+  const dataCurrentAccommodation = datas.filter(
+    (data) => data.id === idAccommodation
   );
-}
 
-export default Accommodation
+  useEffect(() => {
+    const dataCurrentAccommodation = datas.filter(
+      (data) => data.id === idAccommodation
+    );
+    setImageSlider(dataCurrentAccommodation[0]?.pictures || []);
+  }, [idAccommodation]);
+
+  const name = dataCurrentAccommodation[0]?.host.name.split(" ") || [];
+  const rating = dataCurrentAccommodation[0]?.rating || 0;
+  const description = dataCurrentAccommodation[0]?.description;
+  const equipments = dataCurrentAccommodation[0]?.equipments;
+
+  useEffect(() => {
+    const isIdValid = datas.some((data) => data.id === idAccommodation);
+    if (!isIdValid) {
+      navigate("/NotFound");
+    }
+  }, [idAccommodation, navigate]);
+
+  return (
+    <>
+      <Header />
+      <Slider imageSlider={imageSlider} />
+      <main className="accommodation">
+        <div className="accommodation_content">
+          <div className="accommodation_content_infos">
+            <h1>{dataCurrentAccommodation[0]?.title}</h1>
+            <p>{dataCurrentAccommodation[0]?.location}</p>
+            <div>
+              {dataCurrentAccommodation[0]?.tags?.map((tag, index) => {
+                return <button key={index}>{tag}</button>;
+              })}
+            </div>
+          </div>
+          <div className="accommodation_content_host">
+            <div>
+              <div className="accommodation_content_host_name">
+                <span>{name[0]}</span>
+                <span>{name[1]}</span>
+              </div>
+              <img
+                src={dataCurrentAccommodation[0]?.host.picture}
+                alt="host of this accommodation"
+              />
+            </div>
+
+            <div className="accommodation_content_host_stars">
+              {[...Array(5)].map((star, index) => {
+                const ratingValue = index + 1;
+                return (
+                  <img
+                    key={index}
+                    src={ratingValue <= rating ? redStar : greyStar}
+                    alt="star"
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="accommodation_collapse">
+          <div className="accommodation_collapse_item">
+            <Collapse title={"Description"} content={description} />
+          </div>
+          <div className="accommodation_collapse_item">
+            <Collapse title={"Équipements"} content={equipments} />
+          </div>
+        </div>
+      </main>
+      <Footer />
+      </>
+  )
+            }
